7c8bb2a164f02ec97454ad114cef65c877984516
petrushy <petrus.hyvonen@gmail.com>
2018-10-17 07:30:48 +0200

    Mute output of activate scripts

--- recipe/scripts/activate.bat
+++ recipe/scripts/activate.bat
@@ -2,9 +2,9 @@
 :: so other installs don't pollute the environment.
 
 @if defined JCC_JDK (
-    set "_JCC_JDK_CONDA_BACKUP=%JCC_JDK%"
+    @set "_JCC_JDK_CONDA_BACKUP=%JCC_JDK%"
 )
-set "JCC_JDK=%CONDA_PREFIX%\Library"
+@set "JCC_JDK=%CONDA_PREFIX%\Library"
 
 @set "_JCC_PATH_CONDA_BACKUP=%PATH%"
 @set "PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%;%JCC_JDK%\jre\bin;%PATH%"
