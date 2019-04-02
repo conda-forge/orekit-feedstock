8227195f01395de79f87c52b8855f24a94fe90a7
Petrus Hyvönen <petrus.hyvonen@sscspace.com>
2019-04-02 10:17:32 +0200

    Version for orekit 9.3.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ $PYTHON -m jcc \
 --use_full_names \
 --python orekit \
 --version ${PKG_VERSION} \
---jar $SRC_DIR/orekit-9.3.jar \
+--jar $SRC_DIR/orekit-9.3.1.jar \
 --jar $SRC_DIR/hipparchus-core-1.4.jar \
 --jar $SRC_DIR/hipparchus-filtering-1.4.jar \
 --jar $SRC_DIR/hipparchus-fitting-1.4.jar \
@@ -58,6 +58,7 @@ java.util.concurrent.atomic.AtomicReference \
 --reserved min \
 --reserved max \
 --reserved mean \
+--reserved SNAN \
 --files 10 \
 --build \
 --install
