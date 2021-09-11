ad3c015cf9de02c8205175900dd765b67be8410b
Petrus Hyv?nen <petrus.hyvonen@gmail.com>
2021-09-11 23:41:34 +0200

    WIP v11

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,6 +32,7 @@ $PYTHON -m jcc \
 --version ${PKG_VERSION} \
 --jar $SRC_DIR/orekit-11.0.jar \
 --jar $SRC_DIR/hipparchus-core-2.0.jar \
+--jar $SRC_DIR/hipparchus-fitting-2.0.jar \
 --jar $SRC_DIR/hipparchus-filtering-2.0.jar \
 --jar $SRC_DIR/hipparchus-geometry-2.0.jar \
 --jar $SRC_DIR/hipparchus-ode-2.0.jar \
