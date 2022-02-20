dcac6921a14ceb67a6068604f5bf98fe0cc24621
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2022-02-20 20:44:48 +0100

    Updated PythonSupplier

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -56,6 +56,7 @@ java.util.stream.DoubleStream ^
 java.util.function.LongConsumer ^
 java.util.function.IntConsumer ^
 java.util.function.DoubleConsumer ^
+java.util.function.Supplier ^
 --module %SRC_DIR%\pyhelpers.py ^
 --reserved INFINITE ^
 --reserved ERROR ^
@@ -68,6 +69,7 @@ java.util.function.DoubleConsumer ^
 --reserved SNAN ^
 --reserved PASCAL ^
 --reserved PERCENT ^
+--reserved get ^
 --classpath %PREFIX%\Library\lib\tools.jar ^
 --files 81 ^
 --build ^
